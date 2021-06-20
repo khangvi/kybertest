7e1ea38c264dcce4cb83736169ec731079d42a01
khangvi <lamkhangviiii@gmail.com>
2021-06-20 19:04:47 +0700

    first commit

---
+++ 5.checkRates.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+var Web3 = require("web3")
+var FPR = require("kyber-fpr-sdk")
+var addresses = require("./addresses.json")
+const KTTokenAddress = "0xaD6D458402F60fD3Bd25163575031ACDce07538D"
+const provider = new Web3.providers.HttpProvider(process.env.TESTNET_NODE_URL)
+const web3 = new Web3(provider)
+const reserveManager = new FPR.Reserve(web3, addresses)
+reserveManager.getBuyRates(KTTokenAddress,1).then(result => {console.log(result)})
+reserveManager.getSellRates(KTTokenAddress,1).then(result => {console.log(result)})
